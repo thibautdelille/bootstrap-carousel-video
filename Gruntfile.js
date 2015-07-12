31641c8076f691bc30c2e702b2163b28b838cc97
Thibaut Delille <tdelille@gmail.com>
2015-07-12 11:05:18 -0700

    move index into root

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,30 @@
 module.exports = function(grunt) {
 
   grunt.initConfig({
-
+    connect: {
+      server: {
+        options: {
+          port: 3000,
+          base: '.',
+          keepalive: true
+        }
+      }
+    },
+    copy: {
+      // Copy all ./assets/ folder to Jekyll folder
+      bower: {
+        files: [
+          {
+            expand: true,
+            cwd: '.',
+            src: ['/bower_components/bootstrap/dist/css/bootstap.min.css'],
+            dest: 'src' }
+        ]
+      },
+    }
   });
-  
+
+  grunt.registerTask('server', ['connect:server']);
+
   require('matchdep').filterDev('grunt-contrib*').forEach(grunt.loadNpmTasks);
 }
